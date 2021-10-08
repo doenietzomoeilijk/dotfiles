5b8d48b4eb10aeb9619e56c997091ccf4486f372
Max Roeleveld <doenietzomoeilijk@gmail.com>
2021-10-08 22:18:28 +0200

    Refactor into new structure.

---
+++ scripts/polybar-launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar secondary 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bars launched..."
